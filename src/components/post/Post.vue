af2d49ea072cc4d13109ab88aa47f4e19ab8f12f
Maronato <gustavomaronato@gmail.com>
2020-07-20 23:54:13 -0300

    only show TOC on large posts

--- src/components/post/Post.vue
+++ src/components/post/Post.vue
@@ -53,7 +53,7 @@ export default defineComponent({
       updateToc()
     })
     const showToc = computed(() =>
-      !props.post.reading_time ? false : props.post.reading_time < 10
+      !props.post.reading_time ? false : props.post.reading_time >= 10
     )
 
     const aside = computed(() => {
