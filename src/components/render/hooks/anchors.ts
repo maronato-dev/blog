e2696b3664cbb88dc25aba1a2eacc4d95e6233e1
Maronato <gustavomaronato@gmail.com>
2020-08-07 16:51:36 -0300

    Only add anchors if they do not exist

--- src/components/render/hooks/anchors.ts
+++ src/components/render/hooks/anchors.ts
@@ -2,19 +2,21 @@ import { ref, onMounted, getCurrentInstance } from "vue"
 
 const anchorHeadings = (el: HTMLElement) => {
   el.querySelectorAll("h1,h2,h3,h4").forEach(heading => {
-    const anchor = document.createElement("a")
-    anchor.setAttribute("class", "heading-anchor")
-    anchor.setAttribute("href", `#${heading.id}`)
-    anchor.textContent = "#"
-    heading.appendChild(anchor)
+    const anchorId = `${heading.id}_anchor`
+    if (!document.getElementById(anchorId)) {
+      const anchor = document.createElement("a")
+      anchor.setAttribute("class", "heading-anchor")
+      anchor.setAttribute("href", `#${heading.id}`)
+      anchor.setAttribute("id", anchorId)
+      anchor.textContent = "#"
+      heading.appendChild(anchor)
+    }
   })
 }
 
 export const useHeadingAnchors = () => {
   const hasAnchors = ref(false)
-  console.log("USING ANCHORS", hasAnchors.value)
   onMounted(() => {
-    console.log("MOUNTING ANCHORS", hasAnchors.value)
     const vm = getCurrentInstance()
     if (!vm) throw "Component instance not available!"
 
@@ -23,7 +25,6 @@ export const useHeadingAnchors = () => {
       el = el.parentNode as HTMLElement
     }
     if (!hasAnchors.value) {
-      console.log("CREATING ANCHORS", hasAnchors.value)
       anchorHeadings(el)
       hasAnchors.value = true
     }
