204bd258231a0076b39d2cb82ef51dfe03215c29
Maronato <gustavomaronato@gmail.com>
2020-09-06 20:29:21 -0300

    finished footer

--- src/components/layout/BlogFooter.vue
+++ src/components/layout/BlogFooter.vue
@@ -3,32 +3,39 @@
     class="relative bg-white py-6 px-8 dark:bg-gray-900 dark:text-gray-100 w-full z-10 border-t border-gray-300 dark:border-gray-800"
   >
     <div
-      class="container mx-auto flex items-center justify-around md:justify-between flex-wrap flex-col md:flex-row content-around"
+      class="container mx-auto flex items-center text-center justify-around md:justify-between flex-wrap flex-col md:flex-row content-around"
     >
-      <div class>Footer left</div>
       <div>
-        <span class="label ml-1">&copy; {{ year }}.</span>
-        <span class="label ml-1">Made by</span>
-        <span class="value ml-1">Maronato</span>
+        <span class="text-gray-600 dark:text-gray-600 ml-1">
+          &copy; {{ year }}.
+        </span>
+        <span class="text-gray-600 dark:text-gray-600 ml-1">
+          {{ madeByText }}
+        </span>
+        <a class="ml-1" href="https://shrt.maronato.dev/github">Maronato</a>
+        <span class="text-gray-600 dark:text-gray-600">.</span>
+      </div>
+      <div>
+        <a href="https://shrt.maronato.dev/gh-blog">GitHub</a>
+        <span class="mx-1 my-0 opacity-50">•</span>
+        <router-link to="/privacy">{{ pivacyPolicyText }}</router-link>
       </div>
     </div>
   </footer>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue"
+import { computed, defineComponent } from "vue"
+import { useI18n } from "vue-i18n"
 export default defineComponent(() => {
   const date = new Date()
   const year = date.getFullYear()
 
-  return { year }
+  const i18n = useI18n()
+  const pivacyPolicyText = computed(() => i18n.t("footer.privacy-policy"))
+
+  const madeByText = computed(() => i18n.t("footer.made-by"))
+
+  return { year, pivacyPolicyText, madeByText }
 })
 </script>
-<style lang="postcss" scoped>
-.label {
-  @apply text-gray-600;
-  @nest .dark-mode & {
-    @apply text-gray-600;
-  }
-}
-</style>
