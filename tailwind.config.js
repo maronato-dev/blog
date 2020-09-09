6ded1196f72c98824a18105507080723a59eb9b8
Maronato <gustavomaronato@gmail.com>
2020-09-09 09:53:11 -0300

    remove purge whitelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -182,10 +182,6 @@ module.exports = {
   ],
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === "production",
     content: ["./src/**/*.vue", "./src/**/*.ts", "./index.html"],
-    options: {
-      whitelist: ["dark-mode"],
-    },
   },
 }
